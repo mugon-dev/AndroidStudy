2506694d4c04abaffbb3860ac6c98f87511c46a3
mugon-dev <68521263+mugon-dev@users.noreply.github.com>
2020-08-11 13:56:18 +0900

    finish

--- TimeDateWidgetApplication/app/src/main/java/com/example/timedatewidgetapplication/MainActivity.java
+++ TimeDateWidgetApplication/app/src/main/java/com/example/timedatewidgetapplication/MainActivity.java
@@ -2,13 +2,82 @@ package com.example.timedatewidgetapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    Chronometer chronometer;
+    RadioButton rbCal, rbTime;
+    DatePicker datePicker;
+    TimePicker timePicker;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chronometer=findViewById(R.id.Chronometer1);
+        rbCal=findViewById(R.id.rbCal);
+        rbTime=findViewById(R.id.rbTime);
+        datePicker=findViewById(R.id.datePicker);
+        timePicker=findViewById(R.id.timePicker);
+        tvResult=findViewById(R.id.tvResult);
+
+        rbCal.setVisibility(View.INVISIBLE);
+        rbTime.setVisibility(View.INVISIBLE);
+        datePicker.setVisibility(View.INVISIBLE);
+        timePicker.setVisibility(View.INVISIBLE);
+
+        rbCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker.setVisibility(View.VISIBLE);
+                timePicker.setVisibility(View.INVISIBLE);
+            }
+        });
+        rbTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker.setVisibility(View.INVISIBLE);
+                timePicker.setVisibility(View.VISIBLE);
+            }
+        });
+        chronometer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rbCal.setVisibility(View.VISIBLE);
+                rbTime.setVisibility(View.VISIBLE);
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+                chronometer.setTextColor(Color.RED);
+            }
+        });
+        datePicker.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                chronometer.stop();
+                chronometer.setTextColor(Color.BLUE);
+                String str=datePicker.getYear()+"년 ";
+                str+=datePicker.getMonth()+"월 ";
+                str+=datePicker.getDayOfMonth()+"일 ";
+                str+=timePicker.getCurrentHour()+"시 ";
+                str+=timePicker.getCurrentMinute()+"분 ";
+                tvResult.setText(str);
+                rbCal.setVisibility(View.INVISIBLE);
+                rbTime.setVisibility(View.INVISIBLE);
+                datePicker.setVisibility(View.INVISIBLE);
+                timePicker.setVisibility(View.INVISIBLE);
+
+                return true;
+            }
+        });
     }
 }
