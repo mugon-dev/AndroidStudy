3cb707bda2d261831f203c604823c45d2e8ad860
mugon-dev <68521263+mugon-dev@users.noreply.github.com>
2020-08-11 12:23:01 +0900

    날짜/시간 위젯

--- MyTestApplication/app/src/main/java/com/example/mytestapplication/MainActivity.java
+++ MyTestApplication/app/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mytestapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
@@ -26,6 +30,60 @@ public class MainActivity extends AppCompatActivity {
         btnStart=findViewById(R.id.btnStart);
         btnEnd=findViewById(R.id.btnEnd);
         chronometer=findViewById(R.id.chronometer1);
-        //hellow
+        rbCal=findViewById(R.id.rbCal);
+        rbTime=findViewById(R.id.rbTime);
+        calendarView=findViewById(R.id.calendarView);
+        timePicker=findViewById(R.id.timePicker);
+        tvResult=findViewById(R.id.tvResult);
+
+        timePicker.setVisibility(View.INVISIBLE);
+        calendarView.setVisibility(View.INVISIBLE);
+
+        rbCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePicker.setVisibility(View.INVISIBLE);
+                calendarView.setVisibility(View.VISIBLE);
+            }
+        });
+        rbTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePicker.setVisibility(View.VISIBLE);
+                calendarView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+                chronometer.setTextColor(Color.RED);
+            }
+        });
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.stop();
+                chronometer.setTextColor(Color.BLUE);
+                String str=selectYear+"년 ";
+                str+=selectMonth+"월 ";
+                str+=selectDay+"일 ";
+                str+=timePicker.getCurrentHour()+"시 ";
+                str+=timePicker.getCurrentMinute()+"분 예약됨";
+                tvResult.setText(str);
+            }
+        });
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                selectYear=year;
+                //month는 0부터 시작
+                selectMonth=month+1;
+                selectDay=day;
+            }
+        });
+
     }
 }
