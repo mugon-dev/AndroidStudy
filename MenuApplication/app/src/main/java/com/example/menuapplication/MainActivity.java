73daf86881a005d4c6531f125293319746ac4a9b
mugon-dev <68521263+mugon-dev@users.noreply.github.com>
2020-08-12 11:30:27 +0900

    menu
    클릭할때마다 배경색변경, 버튼회전,버튼확대

---
+++ MenuApplication/app/src/main/java/com/example/menuapplication/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.menuapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout linearLayout;
+    Button button;
+    int rotate;
+    int scale;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("메뉴사용");
+        linearLayout=findViewById(R.id.linerLayout1);
+        button=findViewById(R.id.button);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        //지금 현재 액티비티가 가지고 있는 데뉴인플레터 가져오기
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu1,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemRed:
+                linearLayout.setBackgroundColor(Color.RED);
+                break;
+            case R.id.itemGreen:
+                linearLayout.setBackgroundColor(Color.GREEN);
+                break;
+            case R.id.itemBlue:
+                linearLayout.setBackgroundColor(Color.BLUE);
+                break;
+            case R.id.subRotate:
+                rotate+=10;
+                if(rotate>=360) rotate=0;
+                button.setRotation(rotate);
+                break;
+            case R.id.subSizeE:
+                scale*=2;
+                if(scale>10)scale=10;
+                button.setScaleX(scale);
+                button.setScaleY(scale);
+                break;
+            case R.id.subSizeC:
+                scale/=2;
+                if(scale<=1)scale=1;
+                button.setScaleX(scale);
+                button.setScaleY(scale);
+                break;
+        }
+        //메뉴선택이 안되었을때
+        return true;
+    }
+}
