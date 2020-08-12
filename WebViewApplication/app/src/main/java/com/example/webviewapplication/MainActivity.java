6275c4f4d4c9f1fb97918aa47a05c73cdbb37af3
mugon-dev <68521263+mugon-dev@users.noreply.github.com>
2020-08-12 10:27:30 +0900

    webview
    외부 인터넷 연결

--- WebViewApplication/app/src/main/java/com/example/webviewapplication/MainActivity.java
+++ WebViewApplication/app/src/main/java/com/example/webviewapplication/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.webviewapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView wv;
+    Button btnGo, btnBack;
+    EditText ed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wv=findViewById(R.id.webView1);
+        btnGo=findViewById(R.id.btnGo);
+        btnBack=findViewById(R.id.btnBack);
+        ed=findViewById(R.id.edUrl);
+        //1. webview client 설정
+        wv.setWebViewClient(new WebViewClient1());
+        //2. webview 세팅값 가져오기
+        WebSettings webSettings = wv.getSettings();
+        //3. webview 줌인 추가
+        webSettings.setBuiltInZoomControls(true);
+        //4. manifest에서 외부 인터넷 사용권한 추가
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wv.loadUrl(ed.getText().toString());
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wv.goBack();
+            }
+        });
+    }
+    class WebViewClient1 extends android.webkit.WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
     }
 }
