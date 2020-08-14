c7f0fb47648529b1de1422afd044b78f641671d4
mugon-dev <68521263+mugon-dev@users.noreply.github.com>
2020-08-14 14:50:22 +0900

    lifeCycle

---
+++ lifeCycleApplication/app/src/main/java/com/example/lifecycleapplication/SecondActivity.java
@@ -0,0 +1,66 @@
+package com.example.lifecycleapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.i("second 액티비티 테스트: ","onCreate()");
+
+        btn=findViewById(R.id.btnFinish_s);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("second 액티비티 테스으: ","onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("second 액티비티 테스으: ","onResume()");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("second 액티비티 테스으: ","onRestart()");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("second 액티비티 테스으: ","onPause()");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("second 액티비티 테스으: ","onStop()");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("second 액티비티 테스으: ","onDestroy()");
+
+    }
+}
